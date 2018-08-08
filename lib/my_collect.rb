87e79b6eca064a4049de256bba52e0486a3a0c1f
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 14:23:16 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+#require 'pry'
+#collection = ['ruby', 'javascript', 'python', 'objective-c']
+#collection = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+def my_collect(collection)
+  i=0
+  array=[]
+  while i < collection.size
+    array.push(yield(collection[i]))
+    i+=1
+  end
+  array
+#binding.pry
+end
 
-
+collection=[]
+my_collect(collection) do |name|
+  if name.split.size == 1
+  name.upcase
+  else
+    name.split[0]
+  end
+end
